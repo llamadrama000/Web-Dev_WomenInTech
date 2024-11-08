cdf32239d903813515fd8f073d393a3852287b78
melissagio <melmariegio@gmail.com>
2024-11-08 02:20:30 -0800

    melissa_navBar
    just need to add drop down error next to about and slightly make logo bigger

--- WIT-website/src/NavBar.jsx
+++ WIT-website/src/NavBar.jsx
@@ -3,25 +3,30 @@ import './Styles/NavBar.css';
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
+        <nav className="navBar">
             <div className="navbar-leftside">
                 <a href="/" className="logo">
-                WIT.
+                    WIT<span className="pink-dot">.</span>
                 </a>
             </div>
 
             <div className="navbar-right">
                 <ul className="nav-links">
-                <li className="nav-item">
-                    <a href="/about" className="nav-link">About</a>
-                    <div className="dropdown">
-                        <a href="/about/aboutUs">About Us</a>
-                        <a href="/about/team">Meet Our Team!</a>
-                    </div>
-                </li>
-                <li><a href="/events">Events</a></li>
-                <li><a href="/join">Join Us</a></li>
-                <li><a href="/contact">Contact</a></li>
+                    <li className="nav-item">
+                        <div className="dropdown">
+                            <button className='dropbutton'>
+                                About
+                                <i className='caret-down'></i>
+                            </button>
+                            <div className='dropdown-content'>
+                                <a href="/about/aboutUs">About Us</a>
+                                <a href="/about/team">Meet Our Team!</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li><a href="/events" className="nav-link">Events</a></li>
+                    <li><a href="/join" className="nav-link">Join Us</a></li>
+                    <li><a href="/contact" className="nav-link">Contact</a></li>
                 </ul>
             </div>
         </nav>
