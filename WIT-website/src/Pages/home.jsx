e94c30a962aa6fccd560f45b462e7ce4517899cd
rane20 <145800950+rane20@users.noreply.github.com>
2025-02-06 00:47:31 -0800

    home page progress

--- WIT-website/src/Pages/home.jsx
+++ WIT-website/src/Pages/home.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'; // Add this line
-import '../styles/Home.css'; 
-import logo2 from '../assets/Logo/Women_In_Tech_Logo_pink.png';
+import '../styles/Home.css';
+import logo2 from '../assets/Logo/Women_In_Tech_Logo.png';
 import instagramLogo from '../assets/Logo/instaPurple.png';
-import instagramLogo2 from '../assets/Logo/InstaWritingLogo.webp';
 
 import discordLogo from '../assets/Logo/discordPurple.png';
-import discordLogo2 from '../assets/Logo/DiscordWritingLogo.png';
+
 
 import MembershipLogo from '../assets/Logo/MembershipLogo.png';
 
@@ -33,7 +32,6 @@ function Home() {
           alt="Women in Tech Logo"
           className="wit-logo2"
         />
-        <div className="purpleDot"></div>
       </div>
 
       <div className="carousel-container">
@@ -47,8 +45,8 @@ function Home() {
                 backgroundImage: `url(${image})`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
-                width: '500px',
-                height: '500px',
+                width: '600px',
+                height: '600px',
                 borderRadius: '10px',
               }}
             />
@@ -57,41 +55,31 @@ function Home() {
         <button className="next-btn" onClick={nextImage}>❯</button>
       </div>
 
-{/* Social Media section */}
+      {/* Social Media section */}
 <div className="image-links">
-        {/* Instagram */}
-        <a href="https://www.instagram.com/calpolywit/" target="_blank" rel="noopener noreferrer">
-          <img src={instagramLogo} alt="Instagram" className='Instagram' style={{
-            width: '50px', height: 'auto', borderRadius:'30px',
-          }} />
-        </a>
+  {/* Instagram */}
+  <a href="https://www.instagram.com/calpolywit/" target="_blank" rel="noopener noreferrer" className="social-link">
+    <img src={instagramLogo} alt="Instagram" className="social-logo" />
+    <span className="social-text">Instagram</span>
+  </a>
 
-        {/*left of Instagram*/}
-        <a href="https://www.instagram.com/calpolywit/" target="_blank" rel="noopener noreferrer">
-          <img src={instagramLogo2} alt="Instagram" className="insta-logo2" />
-        </a>
+  {/* Discord */}
+  <a href="https://discord.com/invite/R5nrdnDFTq" target="_blank" rel="noopener noreferrer" className="social-link">
+    <img src={discordLogo} alt="Discord" className="social-logo" />
+    <span className="social-text">Discord</span>
+  </a>
 
-        
-        {/* Discord */}
-        <a href="https://discord.com/invite/R5nrdnDFTq" target="_blank" rel="noopener noreferrer">
-          <img src={discordLogo} alt="Discord" className="discord" style={{
-            width: '50px', height: 'auto', borderRadius:'10px',
-          }} />
+  {/* Membership */}
+  <a href="https://docs.google.com/forms/d/e/1FAIpQLSfFZKpcMzfrsDnQopEoeKCzhD2rrWzPALADaOaxjcTWUdUcRg/viewform" target="_blank" rel="noopener noreferrer" className="social-link">
+    <img src={MembershipLogo} alt="Membership Form" className="social-logo" />
+    <span className="social-text">Membership Form</span>
+  </a>
+</div>
 
-          {/*left of Discrod*/}
-        <a href="https://discord.com/invite/R5nrdnDFTq" target="_blank" rel="noopener noreferrer">
-          <img src={discordLogo2} alt="Instagram" className="discord-logo2" />
-        </a>
-        </a>
-        
-      {/* Membership */}
-      <a href="https://docs.google.com/forms/d/e/1FAIpQLSfFZKpcMzfrsDnQopEoeKCzhD2rrWzPALADaOaxjcTWUdUcRg/viewform" target="_blank" rel="noopener noreferrer">
-          <img src={MembershipLogo} alt="Membership Form" className="membership" style={{
-            width: '50px', height: 'auto', borderRadius:'10px',  marginTop: '20px',
-          }} />
-        </a>
-        <span className="membership-text">Membership Form</span>
-      </div>
+
+
+
+
 
 
     </div>
@@ -99,4 +87,5 @@ function Home() {
 }
 
 export default Home;
+
 
